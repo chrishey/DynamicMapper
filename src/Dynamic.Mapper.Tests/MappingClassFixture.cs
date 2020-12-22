df70fb3fa0c0f081d98cc498e05756af69ca6f0b
Chris Hey <chris.hey@slicedbread.co.uk>
2020-12-22 16:07:50 +0000

    Initial classes and test

---
+++ src/Dynamic.Mapper.Tests/MappingClassFixture.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Should;
+using Xunit;
+
+namespace Dynamic.Mapper.Tests
+{
+    public class MappingClassFixture
+    {
+        private readonly MappingClass _itemUnderTest;
+        private MappingConfig _config;
+        private Dictionary<string, object> _payload;
+
+        public MappingClassFixture()
+        {
+            _itemUnderTest = new MappingClass();
+        }
+
+        [Fact]
+        public void Map_Should_Return_Model()
+        {
+            SetupValidPayload();
+            SetupValidConfig();
+
+            var result = _itemUnderTest.Map(_payload, _config);
+
+            result.ShouldNotBeNull();
+        }
+
+        private void SetupValidPayload()
+        {
+            _payload = new Dictionary<string, object>
+            {
+                {"firstname", "Bob" },
+                {"surname", "Bobbins" }
+            };
+        }
+
+        private void SetupValidConfig()
+        {
+            _config = new MappingConfig
+            {
+                MappingDictionary = new Dictionary<string, string> {{"name", "firstname,surname"}}
+            };
+
+        }
+    }
+}
---
+++ src/Dynamic.Mapper/MappingClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dynamic.Mapper
+{
+    public class MappingClass
+    {
+        public EndModel Map(Dictionary<string, object> payload, MappingConfig config)
+        {
+            return new EndModel();
+        }
+    }
+
+    public class EndModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string AddressLine1 { get; set; }
+    }
+
+    public class MappingConfig
+    {
+        public Dictionary<string, string> MappingDictionary { get; set; }
+    }
+}
